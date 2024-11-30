9b553d20f580e1b2837b6142a26b568d50d75f08
szymon-bosiak <szymon.bosiak.99@gmail.com>
2024-11-30 18:07:28 +0100

    feat: footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,30 @@
-import './footer.css'
+import "./footer.css"
 
 function Footer() {
   return (
-    <footer>
-      <p>&copy; 2024 Monbo. All rights reserved.</p>
+    <footer className="footer">
+      <div className="footer_decorator"></div>
+      <div className="footer_content">
+        <p>
+          Website made by{" "}
+          <a
+            href="https://szymon-bosiak.github.io/Portfolio/"
+            className="footer_content-link"
+          >
+            Monbo
+          </a>
+        </p>
+        <p>
+          This website is a fan project and is not affiliated with or endorsed
+          by Blizzard Entertainment®.
+        </p>
+        <p>
+          Overwatch® and all related materials are the property of Blizzard
+          Entertainment®.
+        </p>
+      </div>
     </footer>
-  );
+  )
 }
 
-export default Footer;
+export default Footer
