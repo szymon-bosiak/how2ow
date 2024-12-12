99738d7c595db623a4c0e7c2d4d63a392a6d3ce6
szymon-bosiak <szymon.bosiak.99@gmail.com>
2024-12-12 13:56:27 +0100

    feat: home paralax v1

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,19 +4,9 @@ import "./home.css"
 function Home() {
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 })
   const [isMouseInside, setIsMouseInside] = useState(false)
-  const [isFirstMouseMove, setIsFirstMouseMove] = useState(true)
 
   const assets = {
     lg: {
-      tracer:
-        "https://blz-contentstack-images.akamaized.net/v3/assets/blt2477dcaf4ebd440c/bltf38ff705499bfd25/660c5f692c8f66429a8483e2/Tracer.png?format=webply&quality=90",
-      mei: "https://blz-contentstack-images.akamaized.net/v3/assets/blt2477dcaf4ebd440c/blt0e09c46a0eba2025/660c5f0e194871210a5d1ce1/Mei_Without_Ice_Blast.png?format=webply&quality=90",
-      reinhardt:
-        "https://blz-contentstack-images.akamaized.net/v3/assets/blt2477dcaf4ebd440c/bltdf9dece108257e84/660c5ed039a9b3cb99f86c70/Reinhardt.png?format=webply&quality=90",
-      mercy:
-        "https://blz-contentstack-images.akamaized.net/v3/assets/blt2477dcaf4ebd440c/blt50b24d1663ede72d/660c5ea2940fef074481def0/Edited_Mercy.png?format=webply&quality=90",
-      lucio:
-        "https://blz-contentstack-images.akamaized.net/v3/assets/blt2477dcaf4ebd440c/blt24bd0c583f02e676/660c5e6339a9b3a823f86c6c/Lucio.png?format=webply&quality=90",
       all: "https://blz-contentstack-images.akamaized.net/v3/assets/blt2477dcaf4ebd440c/blt10ca78298ef6edf0/660c5d270b9c8248765adc18/Desktop_Outro_Characters.png?format=webply&quality=90",
       background:
         "https://blz-contentstack-images.akamaized.net/v3/assets/blt2477dcaf4ebd440c/blt0783c76a58bd39e2/660c5cce39a9b3d622f86c68/Desktop_Outro_Sky.jpg?format=webply&quality=90",
@@ -61,12 +51,10 @@ function Home() {
     const y = (e.clientY - rect.top) / rect.height - 0.5
     setMousePosition({ x, y })
     setIsMouseInside(true)
-    setIsFirstMouseMove(false)
   }
 
   const handleMouseLeave = () => {
     setIsMouseInside(false)
-    setIsFirstMouseMove(true)
   }
 
   return (
@@ -83,15 +71,11 @@ function Home() {
             alt={hero.name}
             style={{
               transform: isMouseInside
-                ? `translate3d(${-mousePosition.x * hero.depth * 100}px, ${
-                    -mousePosition.y * hero.depth * 100
+                ? `translate3d(${mousePosition.x * hero.depth * 200}px, ${
+                    mousePosition.y * hero.depth * 200
                   }px, 0px)`
                 : "translate3d(0px, 0px, 0px)",
-              transition: isMouseInside
-                ? isFirstMouseMove
-                  ? "transform 0.s ease-out"
-                  : "none"
-                : "transform 0.6s ease-out",
+              transition: isMouseInside ? "none" : "transform 0.6s ease-out",
             }}
           />
         ))}
